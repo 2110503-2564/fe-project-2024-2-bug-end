0c6f1ebd9f73cf6f9a705f99abfc46f8eb39d28e
Natthawit La-ongsiri <maipai2006@gmail.com>
2025-03-25 19:48:59 +0700

    change be url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hotel booking",
   description: "make your great trip with us",
-};
+}
 
 export default function RootLayout({
   children,
